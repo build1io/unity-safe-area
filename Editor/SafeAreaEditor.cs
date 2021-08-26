0c05931b97fafead8ba8952965adaebf096c4810
George Markosian <georgemarkosian@gmail.com>
2021-08-26 17:53:52 +0300

    Ability to enabled/disable sides paddings added.

--- Editor/SafeAreaEditor.cs
+++ Editor/SafeAreaEditor.cs
@@ -14,20 +14,59 @@ namespace Build1.UnitySafeArea.Editor
         private SerializedProperty referenceResolution;
         private SerializedProperty referenceResolutionWidthAndHeight;
 
-        private SerializedProperty applicableOffsetPercentage;
-        private SerializedProperty applicableOffsetPixels;
-        private SerializedProperty unapplicableOffsetPercentage;
-        private SerializedProperty unapplicableOffsetPixels;
+        private SerializedProperty topApply;
+        private SerializedProperty topApplicableOffsetPercentage;
+        private SerializedProperty topApplicableOffsetPixels;
+        private SerializedProperty topUnapplicableOffsetPercentage;
+        private SerializedProperty topUnapplicableOffsetPixels;
+
+        private SerializedProperty bottomApply;
+        private SerializedProperty bottomApplicableOffsetPercentage;
+        private SerializedProperty bottomApplicableOffsetPixels;
+        private SerializedProperty bottomUnapplicableOffsetPercentage;
+        private SerializedProperty bottomUnapplicableOffsetPixels;
+
+        private SerializedProperty leftApply;
+        private SerializedProperty leftApplicableOffsetPercentage;
+        private SerializedProperty leftApplicableOffsetPixels;
+        private SerializedProperty leftUnapplicableOffsetPercentage;
+        private SerializedProperty leftUnapplicableOffsetPixels;
+
+        private SerializedProperty rightApply;
+        private SerializedProperty rightApplicableOffsetPercentage;
+        private SerializedProperty rightApplicableOffsetPixels;
+        private SerializedProperty rightUnapplicableOffsetPercentage;
+        private SerializedProperty rightUnapplicableOffsetPixels;
 
         public void OnEnable()
         {
             referenceResolutionSource = serializedObject.FindProperty("source");
             referenceResolution = serializedObject.FindProperty("resolution");
             referenceResolutionWidthAndHeight = serializedObject.FindProperty("resolutionWidthAndHeight");
-            applicableOffsetPercentage = serializedObject.FindProperty("applicableOffsetPercentage");
-            applicableOffsetPixels = serializedObject.FindProperty("applicableOffsetPixels");
-            unapplicableOffsetPercentage = serializedObject.FindProperty("unapplicableOffsetPercentage");
-            unapplicableOffsetPixels = serializedObject.FindProperty("unapplicableOffsetPixels");
+
+            topApply = serializedObject.FindProperty("topApply");
+            topApplicableOffsetPercentage = serializedObject.FindProperty("topApplicableOffsetPercentage");
+            topApplicableOffsetPixels = serializedObject.FindProperty("topApplicableOffsetPixels");
+            topUnapplicableOffsetPercentage = serializedObject.FindProperty("topUnapplicableOffsetPercentage");
+            topUnapplicableOffsetPixels = serializedObject.FindProperty("topUnapplicableOffsetPixels");
+
+            bottomApply = serializedObject.FindProperty("bottomApply");
+            bottomApplicableOffsetPercentage = serializedObject.FindProperty("bottomApplicableOffsetPercentage");
+            bottomApplicableOffsetPixels = serializedObject.FindProperty("bottomApplicableOffsetPixels");
+            bottomUnapplicableOffsetPercentage = serializedObject.FindProperty("bottomUnapplicableOffsetPercentage");
+            bottomUnapplicableOffsetPixels = serializedObject.FindProperty("bottomUnapplicableOffsetPixels");
+
+            leftApply = serializedObject.FindProperty("leftApply");
+            leftApplicableOffsetPercentage = serializedObject.FindProperty("leftApplicableOffsetPercentage");
+            leftApplicableOffsetPixels = serializedObject.FindProperty("leftApplicableOffsetPixels");
+            leftUnapplicableOffsetPercentage = serializedObject.FindProperty("leftUnapplicableOffsetPercentage");
+            leftUnapplicableOffsetPixels = serializedObject.FindProperty("leftUnapplicableOffsetPixels");
+
+            rightApply = serializedObject.FindProperty("rightApply");
+            rightApplicableOffsetPercentage = serializedObject.FindProperty("rightApplicableOffsetPercentage");
+            rightApplicableOffsetPixels = serializedObject.FindProperty("rightApplicableOffsetPixels");
+            rightUnapplicableOffsetPercentage = serializedObject.FindProperty("rightUnapplicableOffsetPercentage");
+            rightUnapplicableOffsetPixels = serializedObject.FindProperty("rightUnapplicableOffsetPixels");
         }
 
         public override void OnInspectorGUI()
@@ -52,23 +91,23 @@ namespace Build1.UnitySafeArea.Editor
                 case ResolutionSource.Resolution:
 
                     EditorGUILayout.PropertyField(referenceResolution);
-                    
+
                     if (safeArea.ReferenceResolutionSet)
                     {
                         GUI.enabled = false;
                         EditorGUILayout.Vector2Field("Reference Resolution", safeArea.GetCurrentReferenceResolution());
                         GUI.enabled = true;
                     }
-                    
+
                     EditorGUILayout.HelpBox("Resolution is a ScriptableObject provided with the SafeArea tool.\nCreate an instance via Assets menu and use it for all SafeAreas across your app. Reference resolution management will be centralized.", MessageType.Info, false);
-                    
+
                     if (!safeArea.ReferenceResolutionSet)
                         EditorGUILayout.HelpBox("Resolution not set.", MessageType.Error, false);
 
                     break;
 
                 case ResolutionSource.CanvasScaler:
-                    
+
                     safeArea.UpdateCanvasScaler();
 
                     if (safeArea.CanvasScalerFound)
@@ -87,11 +126,45 @@ namespace Build1.UnitySafeArea.Editor
                 default:
                     throw new ArgumentOutOfRangeException();
             }
+            
+            EditorGUILayout.PropertyField(topApply, new GUIContent("Apply"));
+            if (topApply.boolValue)
+            {
+                EditorGUILayout.PropertyField(topApplicableOffsetPercentage, new GUIContent("Applicable Offset (%)"));
+                EditorGUILayout.PropertyField(topApplicableOffsetPixels, new GUIContent("Applicable Offset (px)"));
+                EditorGUILayout.PropertyField(topUnapplicableOffsetPercentage, new GUIContent("Unapplicable Offset (%)"));
+                EditorGUILayout.PropertyField(topUnapplicableOffsetPixels, new GUIContent("Unapplicable Offset (px)"));
+            }
 
-            EditorGUILayout.PropertyField(applicableOffsetPercentage);
-            EditorGUILayout.PropertyField(applicableOffsetPixels);
-            EditorGUILayout.PropertyField(unapplicableOffsetPercentage);
-            EditorGUILayout.PropertyField(unapplicableOffsetPixels);
+            EditorGUILayout.PropertyField(bottomApply, new GUIContent("Apply"));
+            if (bottomApply.boolValue)
+            {
+                EditorGUILayout.PropertyField(bottomApplicableOffsetPercentage, new GUIContent("Applicable Offset (%)"));
+                EditorGUILayout.PropertyField(bottomApplicableOffsetPixels, new GUIContent("Applicable Offset (px)"));
+                EditorGUILayout.PropertyField(bottomUnapplicableOffsetPercentage, new GUIContent("Unapplicable Offset (%)"));
+                EditorGUILayout.PropertyField(bottomUnapplicableOffsetPixels, new GUIContent("Unapplicable Offset (px)"));
+            }
+
+            EditorGUILayout.PropertyField(leftApply, new GUIContent("Apply"));
+            if (leftApply.boolValue)
+            {
+                EditorGUILayout.PropertyField(leftApplicableOffsetPercentage, new GUIContent("Applicable Offset (%)"));
+                EditorGUILayout.PropertyField(leftApplicableOffsetPixels, new GUIContent("Applicable Offset (px)"));
+                EditorGUILayout.PropertyField(leftUnapplicableOffsetPercentage, new GUIContent("Unapplicable Offset (%)"));
+                EditorGUILayout.PropertyField(leftUnapplicableOffsetPixels, new GUIContent("Unapplicable Offset (px)"));
+            }
+
+            EditorGUILayout.PropertyField(rightApply, new GUIContent("Apply"));
+            if (rightApply.boolValue)
+            {
+                EditorGUILayout.PropertyField(rightApplicableOffsetPercentage, new GUIContent("Applicable Offset (%)"));
+                EditorGUILayout.PropertyField(rightApplicableOffsetPixels, new GUIContent("Applicable Offset (px)"));
+                EditorGUILayout.PropertyField(rightUnapplicableOffsetPercentage, new GUIContent("Unapplicable Offset (%)"));
+                EditorGUILayout.PropertyField(rightUnapplicableOffsetPixels, new GUIContent("Unapplicable Offset (px)"));
+            }
+
+            EditorGUILayout.Space(10);
+            EditorGUILayout.HelpBox("Applicable offset will be applied if a side is enabled and safe area padding is > 0.\nUnapplicable offset will be applied if a side is enabled but safe area padding is 0.", MessageType.Info, true);
 
             serializedObject.ApplyModifiedProperties();
         }
--- SafeArea.cs
+++ SafeArea.cs
@@ -13,11 +13,29 @@ namespace Build1.UnitySafeArea
         [SerializeField]                                 public Resolution       resolution;
         [SerializeField]                                 public Vector2          resolutionWidthAndHeight;
 
-        [Header("Applicable Offsets"), SerializeField] private Rect    applicableOffsetPercentage;
-        [SerializeField]                               private RectInt applicableOffsetPixels;
+        [Header("Top"), SerializeField] private bool  topApply;
+        [SerializeField]                private float topApplicableOffsetPercentage;
+        [SerializeField]                private float topApplicableOffsetPixels;
+        [SerializeField]                private float topUnapplicableOffsetPercentage;
+        [SerializeField]                private float topUnapplicableOffsetPixels;
 
-        [Header("Unapplicable Offsets"), SerializeField] private Rect    unapplicableOffsetPercentage;
-        [SerializeField]                                 private RectInt unapplicableOffsetPixels;
+        [Header("Bottom"), SerializeField] private bool  bottomApply;
+        [SerializeField]                   private float bottomApplicableOffsetPercentage;
+        [SerializeField]                   private float bottomApplicableOffsetPixels;
+        [SerializeField]                   private float bottomUnapplicableOffsetPercentage;
+        [SerializeField]                   private float bottomUnapplicableOffsetPixels;
+
+        [Header("Left"), SerializeField] private bool  leftApply;
+        [SerializeField]                 private float leftApplicableOffsetPercentage;
+        [SerializeField]                 private float leftApplicableOffsetPixels;
+        [SerializeField]                 private float leftUnapplicableOffsetPercentage;
+        [SerializeField]                 private float leftUnapplicableOffsetPixels;
+
+        [Header("Right"), SerializeField] private bool  rightApply;
+        [SerializeField]                  private float rightApplicableOffsetPercentage;
+        [SerializeField]                  private float rightApplicableOffsetPixels;
+        [SerializeField]                  private float rightUnapplicableOffsetPercentage;
+        [SerializeField]                  private float rightUnapplicableOffsetPixels;
 
         public bool CanvasScalerFound      => _canvasScaler != null;
         public bool ReferenceResolutionSet => resolution != null;
@@ -73,23 +91,61 @@ namespace Build1.UnitySafeArea
         private ResolutionSource _lastResolutionSource;
         private Vector2          _lastResolution;
 
-        private Rect    _applicableOffsetPercentage;
-        private RectInt _applicableOffsetPixels;
-
-        private Rect    _unapplicableOffsetPercentage;
-        private RectInt _unapplicableOffsetPixels;
+        private bool  _lastTopApply;
+        private float _lastTopApplicableOffsetPercentage;
+        private float _lastTopApplicableOffsetPixels;
+        private float _lastTopUnapplicableOffsetPercentage;
+        private float _lastTopUnapplicableOffsetPixels;
+        
+        private bool  _lastBottomApply;
+        private float _lastBottomApplicableOffsetPercentage;
+        private float _lastBottomApplicableOffsetPixels;
+        private float _lastBottomUnapplicableOffsetPercentage;
+        private float _lastBottomUnapplicableOffsetPixels;
+        
+        private bool  _lastLeftApply;
+        private float _lastLeftApplicableOffsetPercentage;
+        private float _lastLeftApplicableOffsetPixels;
+        private float _lastLeftUnapplicableOffsetPercentage;
+        private float _lastLeftUnapplicableOffsetPixels;
+        
+        private bool  _lastRightApply;
+        private float _lastRightApplicableOffsetPercentage;
+        private float _lastRightApplicableOffsetPixels;
+        private float _lastRightUnapplicableOffsetPercentage;
+        private float _lastRightUnapplicableOffsetPixels;
 
         private void Update()
         {
             UpdateReferenceResolution();
-            
+
             if (_lastSafeArea == Screen.safeArea &&
                 _lastResolutionSource == source &&
                 _lastResolution == _referenceResolution &&
-                _applicableOffsetPercentage == applicableOffsetPercentage &&
-                _applicableOffsetPixels.Equals(applicableOffsetPixels) &&
-                _unapplicableOffsetPercentage == unapplicableOffsetPercentage &&
-                _unapplicableOffsetPixels.Equals(unapplicableOffsetPixels))
+                
+                _lastTopApply == topApply &&
+                _lastTopApplicableOffsetPercentage == topApplicableOffsetPercentage &&
+                _lastTopApplicableOffsetPixels == topApplicableOffsetPixels &&
+                _lastTopUnapplicableOffsetPercentage == topUnapplicableOffsetPercentage &&
+                _lastTopUnapplicableOffsetPixels == topUnapplicableOffsetPixels &&
+                
+                _lastBottomApply == bottomApply &&
+                _lastBottomApplicableOffsetPercentage == bottomApplicableOffsetPercentage &&
+                _lastBottomApplicableOffsetPixels == bottomApplicableOffsetPixels &&
+                _lastBottomUnapplicableOffsetPercentage == bottomUnapplicableOffsetPercentage &&
+                _lastBottomUnapplicableOffsetPixels == bottomUnapplicableOffsetPixels &&
+                
+                _lastLeftApply == leftApply &&
+                _lastLeftApplicableOffsetPercentage == leftApplicableOffsetPercentage &&
+                _lastLeftApplicableOffsetPixels == leftApplicableOffsetPixels &&
+                _lastLeftUnapplicableOffsetPercentage == leftUnapplicableOffsetPercentage &&
+                _lastLeftUnapplicableOffsetPixels == leftUnapplicableOffsetPixels &&
+                
+                _lastRightApply == rightApply &&
+                _lastRightApplicableOffsetPercentage == rightApplicableOffsetPercentage &&
+                _lastRightApplicableOffsetPixels == rightApplicableOffsetPixels &&
+                _lastRightUnapplicableOffsetPercentage == rightUnapplicableOffsetPercentage &&
+                _lastRightUnapplicableOffsetPixels == rightUnapplicableOffsetPixels)
                 return;
 
             ApplySafeArea();
@@ -98,29 +154,47 @@ namespace Build1.UnitySafeArea
             _lastResolutionSource = source;
             _lastResolution = _referenceResolution;
 
-            _applicableOffsetPercentage = applicableOffsetPercentage;
-            _applicableOffsetPixels = applicableOffsetPixels;
+            _lastTopApply = topApply;
+            _lastTopApplicableOffsetPercentage = topApplicableOffsetPercentage;
+            _lastTopApplicableOffsetPixels = topApplicableOffsetPixels;
+            _lastTopUnapplicableOffsetPercentage = topUnapplicableOffsetPercentage;
+            _lastTopUnapplicableOffsetPixels = topUnapplicableOffsetPixels;
 
-            _unapplicableOffsetPercentage = unapplicableOffsetPercentage;
-            _unapplicableOffsetPixels = unapplicableOffsetPixels;
+            _lastBottomApply = bottomApply;
+            _lastBottomApplicableOffsetPercentage = bottomApplicableOffsetPercentage;
+            _lastBottomApplicableOffsetPixels = bottomApplicableOffsetPixels;
+            _lastBottomUnapplicableOffsetPercentage = bottomUnapplicableOffsetPercentage;
+            _lastBottomUnapplicableOffsetPixels = bottomUnapplicableOffsetPixels;
+            
+            _lastLeftApply = leftApply;
+            _lastLeftApplicableOffsetPercentage = leftApplicableOffsetPercentage;
+            _lastLeftApplicableOffsetPixels = leftApplicableOffsetPixels;
+            _lastLeftUnapplicableOffsetPercentage = leftUnapplicableOffsetPercentage;
+            _lastLeftUnapplicableOffsetPixels = leftUnapplicableOffsetPixels;
+            
+            _lastRightApply = rightApply;
+            _lastRightApplicableOffsetPercentage = rightApplicableOffsetPercentage;
+            _lastRightApplicableOffsetPixels = rightApplicableOffsetPixels;
+            _lastRightUnapplicableOffsetPercentage = rightUnapplicableOffsetPercentage;
+            _lastRightUnapplicableOffsetPixels = rightUnapplicableOffsetPixels;
         }
-        
+
         private void Reset()
         {
             var rectTransform = GetComponent<RectTransform>();
             rectTransform.anchorMin = Vector2.zero;
             rectTransform.anchorMax = Vector2.one;
         }
-        
+
         #endif
 
         private void ApplySafeArea()
         {
             var safeArea = Screen.safeArea;
-            
+
             var screenWidth = Screen.width;
             var screenHeight = Screen.height;
-            
+
             var scaleHeight = _referenceResolution.y / screenHeight;
             var scaleWidth = _referenceResolution.x / screenWidth;
 
@@ -129,29 +203,46 @@ namespace Build1.UnitySafeArea
             var leftPixels = safeArea.xMin;
             var rightPixels = -(screenWidth - safeArea.xMax);
 
-            var topUnits = topPixel * scaleHeight * 1.2f;
-            if (topPixel != 0)
-                topUnits += applicableOffsetPixels.y + screenHeight * applicableOffsetPercentage.y;
-            else
-                topUnits += unapplicableOffsetPixels.y + screenHeight * unapplicableOffsetPercentage.y;
+            var topUnits = 0F;
+            var bottomUnits = 0F;
+            var leftUnits = 0F;
+            var rightUnits = 0F;
 
-            var bottomUnits = bottomPixels * scaleHeight * 1.2F;
-            if (bottomPixels != 0)
-                bottomUnits += applicableOffsetPixels.height + screenHeight * applicableOffsetPercentage.height;
-            else
-                bottomUnits += unapplicableOffsetPixels.height + screenHeight * unapplicableOffsetPercentage.height;
+            if (topApply)
+            {
+                topUnits = topPixel * scaleHeight * 1.2f;
+                if (topPixel != 0)
+                    topUnits += topApplicableOffsetPixels + screenHeight * topApplicableOffsetPercentage;
+                else
+                    topUnits += topUnapplicableOffsetPixels + screenHeight * topUnapplicableOffsetPercentage;
+            }
 
-            var leftUnits = leftPixels * scaleWidth;
-            if (leftPixels != 0)
-                leftUnits += applicableOffsetPixels.x + screenWidth * applicableOffsetPercentage.x;
-            else
-                leftUnits += unapplicableOffsetPixels.x + screenWidth * unapplicableOffsetPercentage.x;
+            if (bottomApply)
+            {
+                bottomUnits = bottomPixels * scaleHeight * 1.2F;
+                if (bottomPixels != 0)
+                    bottomUnits += bottomApplicableOffsetPixels + screenHeight * bottomApplicableOffsetPercentage;
+                else
+                    bottomUnits += bottomUnapplicableOffsetPixels + screenHeight * bottomUnapplicableOffsetPercentage;
+            }
 
-            var rightUnits = rightPixels * scaleWidth;
-            if (rightPixels != 0)
-                rightUnits -= applicableOffsetPixels.width + screenWidth * applicableOffsetPercentage.width;
-            else
-                rightUnits -= unapplicableOffsetPixels.width + screenWidth * unapplicableOffsetPercentage.width;
+            if (leftApply)
+            {
+                leftUnits = leftPixels * scaleWidth;
+                if (leftPixels != 0)
+                    leftUnits += leftApplicableOffsetPixels + screenWidth * leftApplicableOffsetPercentage;
+                else
+                    leftUnits += leftUnapplicableOffsetPixels + screenWidth * leftUnapplicableOffsetPercentage;
+            }
+
+            if (leftApply)
+            {
+                rightUnits = rightPixels * scaleWidth;
+                if (rightPixels != 0)
+                    rightUnits -= rightApplicableOffsetPixels + screenWidth * rightApplicableOffsetPercentage;
+                else
+                    rightUnits -= rightUnapplicableOffsetPixels + screenWidth * rightUnapplicableOffsetPercentage;
+            }
 
             var rectTransform = GetComponent<RectTransform>();
             rectTransform.offsetMin = new Vector2(leftUnits, bottomUnits);
